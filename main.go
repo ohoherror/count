e3a74d0f7fea12acc645830b2e35a763fa93b6c1
ohoherror <1458251589@qq.com>
2022-11-11 15:41:21 +0800

    count dont match name

--- main.go
+++ main.go
@@ -7,9 +7,27 @@ import (
 )
 
 func main() {
-	lines, err := datafile.GetStrings("votes.txt")
+	lines, err := datafile.GetStrings("C:\\Users\\User\\go\\bin\\votes.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(lines)
+	var names []string
+	var counts []int
+	for _, line := range lines {
+		matched := false
+		for i, name := range names {
+			if name == line {
+				counts[i]++
+				matched = true
+			}
+
+		}
+		if matched == false {
+			names = append(names, line)
+			counts = append(counts, 1)
+		}
+	}
+	for i, name := range names {
+		fmt.Printf("%s:%d\n", name, counts[i])
+	}
 }

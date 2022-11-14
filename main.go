0a6df919028f4d4dbbbd55beb8266f3895e0df26
ohoherror <1458251589@qq.com>
2022-11-14 15:15:54 +0800

    slice map

--- main.go
+++ main.go
@@ -11,23 +11,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var names []string
-	var counts []int
-	for _, line := range lines {
-		matched := false
-		for i, name := range names {
-			if name == line {
-				counts[i]++
-				matched = true
-			}
 
-		}
-		if matched == false {
-			names = append(names, line)
-			counts = append(counts, 1)
-		}
+	counts := make(map[string]int)
+	for _, line := range lines {
+		counts[line]++
 	}
-	for i, name := range names {
-		fmt.Printf("%s:%d\n", name, counts[i])
-	}
+	fmt.Println(counts)
 }
